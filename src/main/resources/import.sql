9370ff57f5ca16d60a7b88b2ab81079130e1c095
JoaoRudolf <joaogrudolf@gmail.com>
2021-12-07 14:59:00 -0300

    Projeto Finalizado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO tb_cliente(nome, dn) VALUES ('Maria', '14/12/2000');
+INSERT INTO tb_cliente(nome, dn) VALUES ('Magali', '13/04/2020');
 
 INSERT INTO tb_compras(cliente_id, total_compra, data_compra) VALUES (1,'100.0', '2021-05-15');
